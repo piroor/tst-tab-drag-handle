234447edf1cd35c6dff1cd14446b66f68ab08d7e
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2024-02-06 23:29:58 +0900

    Fix typo

--- background/background.js
+++ background/background.js
@@ -129,7 +129,7 @@ let mRenderedOnDemand    = false;
 async function registerToTST() {
   try {
     const [TSTVersion] = await Promise.all([
-      browser.runtime.sendMessage(TST_ID, { type: 'vet-version' }),
+      browser.runtime.sendMessage(TST_ID, { type: 'get-version' }),
       browser.runtime.sendMessage(TST_ID, {
         type: 'register-self' ,
         name: browser.i18n.getMessage('extensionName'),
